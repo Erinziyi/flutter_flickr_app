61214e7cacdffe53f9e7a29163fd94bbe6ed074c
unknown <erin@peoplelogy.com>
2019-07-17 10:48:34 +0800

    Update pull refresh

--- lib/app.dart
+++ lib/app.dart
@@ -34,44 +34,47 @@ List<ImageModel>images =[];
   }
 
   Widget imageList(){
-    return ListView.builder(
-      itemCount: images.length,
-      itemBuilder: (context,index){
-       final imageData = images[index];
-       return Card(
-         margin: EdgeInsets.all(10.0),
-         elevation: 8.0,
-         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-         child: Column(
-           children: <Widget>[
-             ClipRRect(borderRadius:BorderRadius.only(
-               topLeft: Radius.circular(8.0),
-               topRight: Radius.circular(8.0),
-             ),
-               child: Image.network(imageData.url,height: 180.0,width:440,fit:BoxFit.fill),
-             ),
-             Container(
-               padding: EdgeInsets.only(bottom:10.0),
+    return RefreshIndicator(
+      child:ListView.builder(
+        itemCount: images.length,
+        itemBuilder: (context,index){
+          final imageData = images[index];
+          return Card(
+            margin: EdgeInsets.all(10.0),
+            elevation: 8.0,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            child: Column(
+              children: <Widget>[
+                ClipRRect(borderRadius:BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
+                  child: Image.network(imageData.url,height: 180.0,width:440,fit:BoxFit.fill),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom:10.0),
 
+                ),
+                Padding(
+
+                  child: Text(imageData.title,
+                    style: TextStyle(
+                        color:Colors.black87,
+                        fontSize: 16.0),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.clip,
+                  ),
+                  padding: EdgeInsets.all(20.0),
+                )
+              ],
             ),
-             Padding(
-               
-               child: Text(imageData.title,
-                 style: TextStyle(
-                   color:Colors.black87,
-                   fontSize: 16.0),
-                  textAlign: TextAlign.center,
-                 overflow: TextOverflow.clip,
-               ),
-               padding: EdgeInsets.all(20.0),
-             )
-           ],
-         ),
 
-       );
-      },
+          );
+        },
+      ),
+      onRefresh: getImages,
+      );
 
-    );
   }
 
   Future<void>getImages()async{
@@ -79,12 +82,12 @@ List<ImageModel>images =[];
     if (response.statusCode == 200){
       final map = json.decode(response.body);
       final imagesJson = map ["items"];
-      images = [];
+//      images = [];
       imagesJson.forEach((image){
         final imageM =ImageModel.fromJson(image);
         setState(() {
-          images.add(imageM);
-          //images.insert(0,imagesM);
+//          images.add(imageM);
+          images.insert(0,imageM);
 
         });
 
